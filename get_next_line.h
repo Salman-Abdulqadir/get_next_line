01114187e610713c01d9458e0f9f2e99329700da
Salman Abdulqadir <sabdulqa@lab1r4s7.42abudhabi.ae>
2023-06-04 21:29:55 +0400

    starting again

--- get_next_line.h
+++ get_next_line.h
@@ -1,37 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sabdulqa <sabdulqa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/04 13:00:43 by sabdulqa          #+#    #+#             */
-/*   Updated: 2023/03/04 16:30:32 by sabdulqa         ###   ########.fr       */
+/*   Created: 2023/03/04 15:20:35 by sabdulqa          #+#    #+#             */
+/*   Updated: 2023/03/04 15:22:32 by sabdulqa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#ifndef GET_NEXT_LINE
+#define GET_NEXT_LINE
 
-char	*get_next_line(int fd)
-{
-	char	*buffer;
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-	read (fd, buffer, 5);
-	
-	return (buffer);
-}
-int	main()
-{
-	int	fd;
-	fd = open("test.txt", O_RDONLY);
-	char *str;
-	if (fd == -1)
-	{
-		printf("an error occured!");
-		return 1;
-	}
-	str = get_next_line(fd);
-	printf("%s", str);
-	free(str);
-	return 0;
-}
+char *get_next_line(int fd);
+#endif
