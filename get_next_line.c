7aec77567ae6f216ed51c357c946136aeaa73e5d
Salman Abdulqadir <sabdulqa@lab2r4s14.42abudhabi.ae>
2023-06-27 18:41:20 +0400

    finished

--- get_next_line.c
+++ get_next_line.c
@@ -5,50 +5,109 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: sabdulqa <sabdulqa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/04 13:00:43 by sabdulqa          #+#    #+#             */
-/*   Updated: 2023/06/11 12:29:05 by sabdulqa         ###   ########.fr       */
+/*   Created: 2023/06/27 15:20:38 by sabdulqa          #+#    #+#             */
+/*   Updated: 2023/06/27 18:39:18 by sabdulqa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
-int	find_line(char *text)
+char	*join(char *s1, char *s2)
 {
-	int	index;
+	char	*joined;
 
-	index = 0;
-	while(text[index])
+	joined = ft_strjoin(s1, s2);
+	free(s1);
+	return (joined);
+}
+
+char	*read_file(int fd, char *file)
+{
+	char	*buffer;
+	int		read_count;
+
+	if (!file)
+		file = ft_calloc(1, sizeof(char));
+	buffer = ft_calloc((size_t)(BUFFER_SIZE + 1), sizeof(char));
+	read_count = 1;
+	while (read_count > 0)
 	{
-		if (text[index] != '\n')
-		{
-			index++;
-			continue;
-		}
-		return index;
+		read_count = read(fd, buffer, BUFFER_SIZE);
+		if (read_count == -1)
+			break ;
+		buffer[read_count] = '\0';
+		file = join(file, buffer);
 	}
-	return 0;
+	return (file);
+}
+
+char	*find_line(char *file)
+{
+	char	*line;
+	int		i;
+
+	if (!file[0])
+		return (NULL);
+	i = 0;
+	while (file[i] && file[i] != '\n')
+		i++;
+	line = malloc(sizeof(char) * (i + 1));
+	line[i + 1] = '\0';
+	if (!line)
+		return (NULL);
+	line = ft_memcpy(line, file, i + 1);
+	return (line);
+}
+
+char	*find_remainder(char *file)
+{
+	char	*remainder;
+	int		i;
+
+	i = 0;
+	while (file[i] && file[i] != '\n')
+		i++;
+	remainder = malloc(sizeof(char) * (ft_strlen(file) - i));
+	remainder[ft_strlen(file) - i] = '\0';
+	if (!remainder)
+		return (NULL);
+	remainder = ft_memcpy(remainder, file + i + 1, ft_strlen(file) - i);
+	free(file);
+	return (remainder);
 }
 
 char	*get_next_line(int fd)
 {
-	static	char	*buffer;
+	static char	*file;
+	char		*line;
 
-	read (fd, buffer, BUFFER_SIZE);
-	return (buffer);
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	if (BUFFER_SIZE > 2147483647)
+		return (NULL);
+	file = read_file(fd, file);
+	if (!file)
+		return (NULL);
+	line = find_line(file);
+	file = find_remainder(file);
+	return (line);
 }
 
 int	main()
 {
-	printf("buffer size = %d\n\n", BUFFER_SIZE);
-	int	fd;
-	fd = open("test.txt", O_RDONLY);
-	char *str;
-	if (fd == -1)
-	{
-		printf("an error occured!");
-		return 1;
+	int        fd;
+    char    *line;
+    char    *line2;
+    char    *line3;   
+	fd = open("file.txt", O_RDONLY);
+	line = get_next_line(fd);
+	while(line != NULL){
+		printf("%s\n", line);
+		line = get_next_line(fd);
 	}
-	str = get_next_line(fd);
-	printf("%s", str);
-	return 0;
+	free(line);
+	// free(line2);
+	// free(line3);
+    return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,19 +6,27 @@
 /*   By: sabdulqa <sabdulqa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/04 15:20:35 by sabdulqa          #+#    #+#             */
-/*   Updated: 2023/06/11 12:24:18 by sabdulqa         ###   ########.fr       */
+/*   Updated: 2023/06/27 18:36:32 by sabdulqa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE
-#define GET_NEXT_LINE
-#define BUFFER_SIZE 42
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/uio.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <fcntl.h>
 
-char *get_next_line(int fd);
+char	*get_next_line(int fd);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *string, int searchedChar);
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t elementCount, size_t elementSize);
+void	*ft_memcpy(void *dst, void *src, size_t n);
+size_t	ft_strlen(const char *theString);
+
 #endif
